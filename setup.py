1dd4cc36840962b0c379adcbab106f35bd4d2dc0
Joshua Harlow <harlowja@gmail.com>
2014-07-19 12:45:07 -0700

    Bump for 0.0.26

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='zake',
-    version='0.0.25',
+    version='0.0.26',
     description='A python package that works to provide a nice set of '
                 'testing utilities for the kazoo library.',
     author="Joshua Harlow",
