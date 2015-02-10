799a555e58354be993a6bba780ef86c920f3692b
Joshua Harlow <harlowja@yahoo-inc.com>
2015-02-10 13:59:32 -0800

    Release 0.1.7

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.rst", "r") as readme:
 
 setup(
     name='zake',
-    version='0.1.6',
+    version='0.1.7',
     description='A python package that works to provide a nice set of '
                 'testing utilities for the kazoo library.',
     author="Joshua Harlow",
